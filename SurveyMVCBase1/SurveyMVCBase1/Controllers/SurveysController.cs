7ef9be30f97f0e7f6b29c6579a6305685fbc2a60
Steve.JIN <jinyue306@gmail.com>
2017-07-09 16:57:41 +0800

    after aection3 updates

--- SurveyMVCBase1/SurveyMVCBase1/Controllers/SurveysController.cs
+++ SurveyMVCBase1/SurveyMVCBase1/Controllers/SurveysController.cs
@@ -1598,7 +1598,7 @@ namespace SurveyMVCBase1.Controllers
                             {
                                 S3ScoreMsg = "您对企业家签证非常了解，个人背景也有一定的基础，但缺乏英国商业知识，可在阅读《商业创业指南》加深了解后，可尝试申请企业家签证。";
                             }
-                            // case - 12 : 英国商业知识得分 0-50, 签证知识得分 60-90, 基本信息得分 60及以上
+                            // case - 12 : 英国商业知识得分 0-50, 签证知识得分 60-90, 基本信息得分 30-50
                             else if (S3Score <= 50 && S2Score >= 60 && S2Score <= 90 && S1Score >= 30 && S1Score <= 50)
                             {
                                 S3ScoreMsg = "您对企业家签证知识有一定的了解，个人背景也有一定的基础，但英国商业知识有待加强，可通过阅读《商业创业指南》加深了解后，再来参加我们的企业家评测。";
@@ -1646,7 +1646,7 @@ namespace SurveyMVCBase1.Controllers
                 return View("Error");
             }
 
-            if (Section4 == "英国创业项目测试 - 独立测试")
+            if (Section4 == "英国创业项目测试 - 独立创业")
             {
                 return RedirectToAction("S4aPage1");
             }
@@ -3166,6 +3166,12 @@ namespace SurveyMVCBase1.Controllers
         public ActionResult S4aPageSum()
         {
             string id = "";
+
+            int S1Score = 0;
+            int S2Score = 0;
+            int S3Score = 0;
+            string S3ScoreMsg = "";
+
             if (Session["viewKey"] != null)
             {
                 id = Session["viewKey"].ToString();
@@ -3183,27 +3189,76 @@ namespace SurveyMVCBase1.Controllers
 
             Survey survey = db.Surveys.Find(id);
 
-            int S1ScoreTest = 0;
-            string S1ScoreTestMsg = "";
-
             if (survey != null)
             {
-                S1ScoreTest = survey.S1Q3Score + survey.S1Q6Score + survey.S1Q10Score + survey.S1Q11Score + survey.S1Q13Score;
-                if (S1ScoreTest > 60)
+                S1Score = survey.S1Q3Score + survey.S1Q6Score + survey.S1Q10Score + survey.S1Q11Score + survey.S1Q13Score;
+                S2Score = survey.S2Q1Score + survey.S2Q2Score + survey.S2Q3Score + survey.S2Q4Score + survey.S2Q5Score + survey.S2Q6Score + survey.S2Q7Score + survey.S2Q8Score + survey.S2Q9Score + survey.S2Q10Score;
+                S3Score = survey.S3Q1Score + survey.S3Q2Score + survey.S3Q3Score + survey.S3Q4Score + survey.S3Q5Score + survey.S3Q6Score + survey.S3Q7Score + survey.S3Q8Score + survey.S3Q9Score + survey.S3Q10Score;
+                // case - 01 : 英国商业知识得分 100, 签证知识得分 100, 基本信息得分 60及以上
+                if (S3Score == 100 && S2Score == 100 && S1Score >= 60)
                 {
-                    S1ScoreTestMsg = "第一部得分大于60分：您对英国企业家移民签证政策充分了解，可以进入下一步评测。Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam tempus pulvinar dictum. Praesent commodo augue vitae massa vestibulum pulvinar id nec mauris. Nam id tellus tincidunt, fringilla massa eu, finibus lectus. Quisque quis elementum ante. Etiam varius aliquam lacus. Pellentesque vitae luctus eros. Nunc maximus sapien sit amet ipsum scelerisque lobortis. Aenean ac vestibulum lacus. Integer bibendum nunc in aliquet commodo. Nunc mollis laoreet felis in faucibus. Integer faucibus ornare libero sed suscipit. Nullam in euismod nunc, eu tempus velit.";
+                    S3ScoreMsg = "您的个人背景，企业家签证知识和商业知识都非常完美，您非常适合申请英国的企业家签证。";
                 }
-                else if (S1ScoreTest >= 30 && S1ScoreTest < 60)
+                // case - 02 : 英国商业知识得分 100, 签证知识得分 60-90, 基本信息得分 60及以上
+                else if (S3Score == 100 && S2Score >= 60 && S2Score <= 90 && S1Score >= 60)
                 {
-                    S1ScoreTestMsg = "第一部分得分在30-60分之间：您对英国企业家移民签证政策基本了解，可以进入下一步评测。Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam tempus pulvinar dictum. Praesent commodo augue vitae massa vestibulum pulvinar id nec mauris. Nam id tellus tincidunt, fringilla massa eu, finibus lectus. Quisque quis elementum ante. Etiam varius aliquam lacus. Pellentesque vitae luctus eros. Nunc maximus sapien sit amet ipsum scelerisque lobortis. Aenean ac vestibulum lacus. Integer bibendum nunc in aliquet commodo. Nunc mollis laoreet felis in faucibus. Integer faucibus ornare libero sed suscipit. Nullam in euismod nunc, eu tempus velit.";
+                    S3ScoreMsg = "您的个人背景和商业知识都非常好，对英国的企业家签证也有一定的了解，请阅读《商业创业指南》，深入了解企业家签证后可申请英国的企业家签证。";
                 }
-                else if (S1ScoreTest < 30)
+                // case - 03 : 英国商业知识得分 100, 签证知识得分 100, 基本信息得分 30-50
+                else if (S3Score == 100 && S2Score == 100 && S1Score >= 30 && S1Score <= 50)
                 {
-                    S1ScoreTestMsg = "第一部分得分在30分以下：您对英国企业家移民签证政策不够了解，建议您充分了解政策后再进入下一步评测。Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam tempus pulvinar dictum. Praesent commodo augue vitae massa vestibulum pulvinar id nec mauris. Nam id tellus tincidunt, fringilla massa eu, finibus lectus. Quisque quis elementum ante. Etiam varius aliquam lacus. Pellentesque vitae luctus eros. Nunc maximus sapien sit amet ipsum scelerisque lobortis. Aenean ac vestibulum lacus. Integer bibendum nunc in aliquet commodo. Nunc mollis laoreet felis in faucibus. Integer faucibus ornare libero sed suscipit. Nullam in euismod nunc, eu tempus velit.";
+                    S3ScoreMsg = "您对商业知识和企业家签证非常了解，在个人背景方面要有针对性的加强，可以咨询专业律师，并做好充分的准备工作后可尝试申请英国的企业家签证。";
+                }
+                // case - 04 : 英国商业知识得分 100, 签证知识得分 60-90, 基本信息得分 30-50
+                else if (S3Score == 100 && S2Score >= 60 && S2Score <= 90 && S1Score >= 30 && S1Score <= 50)
+                {
+                    S3ScoreMsg = "您对商业知识非常了解，个人背景有一些基础，对企业家签证也有一定的了解，可阅读《商业创业指南》加深了解英国企业家签证知识后，可尝试申请企业家签证。";
+                }
+                // case - 05 : 英国商业知识得分 60-90, 签证知识得分 100, 基本信息得分 60及以上
+                else if (S3Score >= 60 && S3Score <= 90 && S2Score == 100 && S1Score >= 60)
+                {
+                    S3ScoreMsg = "您的个人背景和签证知识都非常好，大体的商业知识也已了解，可通过阅读《商业创业指南》加深对英国商业的理解，在做好充分的准备工作后可申请英国的企业家签证。";
+                }
+                // case - 06 : 英国商业知识得分 60-90, 签证知识得分 60-90, 基本信息得分 60及以上
+                else if (S3Score >= 60 && S3Score <= 90 && S2Score >= 60 && S2Score <= 90 && S1Score >= 60)
+                {
+                    S3ScoreMsg = "您的个人背景很好，对商业知识和企业家签证也有一定的了解，可通过阅读《商业创业指南》加深对英国商业和企业家签证的理解，在做好充分的准备工作后可尝试申请英国的企业家签证。";
+                }
+                // case - 07 : 英国商业知识得分 60-90, 签证知识得分 100, 基本信息得分 30-50
+                else if (S3Score >= 60 && S3Score <= 90 && S2Score == 100 && S1Score >= 30 && S1Score <= 50)
+                {
+                    S3ScoreMsg = "您对企业家签证非常了解，对英国商业知识也有一定的了解，个人背景也有一定的基础，请阅读《商业创业指南》在深入了解、提高后可尝试申请企业家签证。";
+                }
+                // case - 08 : 英国商业知识得分 60-90, 签证知识得分 60-90, 基本信息得分 30-50
+                else if (S3Score >= 60 && S3Score <= 90 && S2Score >= 60 && S2Score <= 90 && S1Score >= 30 && S1Score <= 50)
+                {
+                    S3ScoreMsg = "您对英国商业知识、企业家签证都有一定的了解，个人背景也有一定的基础，请阅读《商业创业指南》在深入了解、提高后可尝试申请企业家签证。";
+                }
+                // case - 09 : 英国商业知识得分 0-50, 签证知识得分 100, 基本信息得分 60及以上
+                else if (S3Score <= 50 && S2Score == 100 && S1Score >= 60)
+                {
+                    S3ScoreMsg = "您的个人背景和签证知识都非常好，但缺乏英国的商业知识，请阅读《商业创业指南》，深入了解英国商业知识后可尝试申请英国的企业家签证。";
+                }
+                // case - 10 : 英国商业知识得分 0-50, 签证知识得分 60-90, 基本信息得分 60及以上
+                else if (S3Score <= 50 && S2Score >= 60 && S2Score <= 90 && S1Score >= 60)
+                {
+                    S3ScoreMsg = "您的个人背景很好，对企业家签证也有一定的了解，需加强商业知识，可通过阅读《商业创业指南》加深对英国商业和企业家签证的理解，在做好充分的准备工作后可尝试申请英国的企业家签证。";
+                }
+                // case - 11 : 英国商业知识得分 0-50, 签证知识得分 100, 基本信息得分 30-50
+                else if (S3Score <= 50 && S2Score == 100 && S1Score >= 30 && S1Score <= 50)
+                {
+                    S3ScoreMsg = "您对企业家签证非常了解，个人背景也有一定的基础，但缺乏英国商业知识，可在阅读《商业创业指南》加深了解后，可尝试申请企业家签证。";
+                }
+                // case - 12 : 英国商业知识得分 0-50, 签证知识得分 60-90, 基本信息得分 30-50
+                else if (S3Score <= 50 && S2Score >= 60 && S2Score <= 90 && S1Score >= 30 && S1Score <= 50)
+                {
+                    S3ScoreMsg = "您对企业家签证知识有一定的了解，个人背景也有一定的基础，但英国商业知识有待加强，可通过阅读《商业创业指南》加深了解后，再来参加我们的企业家评测。";
                 }
 
-                ViewBag.Section1ScoreTest = S1ScoreTest;
-                ViewBag.Section2Message = S1ScoreTestMsg;
+                ViewBag.Section1Score = S1Score;
+                ViewBag.Section2Score = S2Score;
+                ViewBag.Section3Score = S3Score;
+                ViewBag.Section3Message = S3ScoreMsg;
 
                 return View("Section4a/S4aPageSum");
             }
@@ -4797,6 +4852,12 @@ namespace SurveyMVCBase1.Controllers
         public ActionResult S4bPageSum()
         {
             string id = "";
+
+            int S1Score = 0;
+            int S2Score = 0;
+            int S3Score = 0;
+            string S3ScoreMsg = "";
+
             if (Session["viewKey"] != null)
             {
                 id = Session["viewKey"].ToString();
@@ -4814,27 +4875,76 @@ namespace SurveyMVCBase1.Controllers
 
             Survey survey = db.Surveys.Find(id);
 
-            int S1ScoreTest = 0;
-            string S1ScoreTestMsg = "";
-
             if (survey != null)
             {
-                S1ScoreTest = survey.S1Q3Score + survey.S1Q6Score + survey.S1Q10Score + survey.S1Q11Score + survey.S1Q13Score;
-                if (S1ScoreTest > 60)
+                S1Score = survey.S1Q3Score + survey.S1Q6Score + survey.S1Q10Score + survey.S1Q11Score + survey.S1Q13Score;
+                S2Score = survey.S2Q1Score + survey.S2Q2Score + survey.S2Q3Score + survey.S2Q4Score + survey.S2Q5Score + survey.S2Q6Score + survey.S2Q7Score + survey.S2Q8Score + survey.S2Q9Score + survey.S2Q10Score;
+                S3Score = survey.S3Q1Score + survey.S3Q2Score + survey.S3Q3Score + survey.S3Q4Score + survey.S3Q5Score + survey.S3Q6Score + survey.S3Q7Score + survey.S3Q8Score + survey.S3Q9Score + survey.S3Q10Score;
+                // case - 01 : 英国商业知识得分 100, 签证知识得分 100, 基本信息得分 60及以上
+                if (S3Score == 100 && S2Score == 100 && S1Score >= 60)
                 {
-                    S1ScoreTestMsg = "第一部得分大于60分：您对英国企业家移民签证政策充分了解，可以进入下一步评测。Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam tempus pulvinar dictum. Praesent commodo augue vitae massa vestibulum pulvinar id nec mauris. Nam id tellus tincidunt, fringilla massa eu, finibus lectus. Quisque quis elementum ante. Etiam varius aliquam lacus. Pellentesque vitae luctus eros. Nunc maximus sapien sit amet ipsum scelerisque lobortis. Aenean ac vestibulum lacus. Integer bibendum nunc in aliquet commodo. Nunc mollis laoreet felis in faucibus. Integer faucibus ornare libero sed suscipit. Nullam in euismod nunc, eu tempus velit.";
+                    S3ScoreMsg = "您的个人背景，企业家签证知识和商业知识都非常完美，您非常适合申请英国的企业家签证。";
                 }
-                else if (S1ScoreTest >= 30 && S1ScoreTest < 60)
+                // case - 02 : 英国商业知识得分 100, 签证知识得分 60-90, 基本信息得分 60及以上
+                else if (S3Score == 100 && S2Score >= 60 && S2Score <= 90 && S1Score >= 60)
                 {
-                    S1ScoreTestMsg = "第一部分得分在30-60分之间：您对英国企业家移民签证政策基本了解，可以进入下一步评测。Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam tempus pulvinar dictum. Praesent commodo augue vitae massa vestibulum pulvinar id nec mauris. Nam id tellus tincidunt, fringilla massa eu, finibus lectus. Quisque quis elementum ante. Etiam varius aliquam lacus. Pellentesque vitae luctus eros. Nunc maximus sapien sit amet ipsum scelerisque lobortis. Aenean ac vestibulum lacus. Integer bibendum nunc in aliquet commodo. Nunc mollis laoreet felis in faucibus. Integer faucibus ornare libero sed suscipit. Nullam in euismod nunc, eu tempus velit.";
+                    S3ScoreMsg = "您的个人背景和商业知识都非常好，对英国的企业家签证也有一定的了解，请阅读《商业创业指南》，深入了解企业家签证后可申请英国的企业家签证。";
                 }
-                else if (S1ScoreTest < 30)
+                // case - 03 : 英国商业知识得分 100, 签证知识得分 100, 基本信息得分 30-50
+                else if (S3Score == 100 && S2Score == 100 && S1Score >= 30 && S1Score <= 50)
                 {
-                    S1ScoreTestMsg = "第一部分得分在30分以下：您对英国企业家移民签证政策不够了解，建议您充分了解政策后再进入下一步评测。Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam tempus pulvinar dictum. Praesent commodo augue vitae massa vestibulum pulvinar id nec mauris. Nam id tellus tincidunt, fringilla massa eu, finibus lectus. Quisque quis elementum ante. Etiam varius aliquam lacus. Pellentesque vitae luctus eros. Nunc maximus sapien sit amet ipsum scelerisque lobortis. Aenean ac vestibulum lacus. Integer bibendum nunc in aliquet commodo. Nunc mollis laoreet felis in faucibus. Integer faucibus ornare libero sed suscipit. Nullam in euismod nunc, eu tempus velit.";
+                    S3ScoreMsg = "您对商业知识和企业家签证非常了解，在个人背景方面要有针对性的加强，可以咨询专业律师，并做好充分的准备工作后可尝试申请英国的企业家签证。";
+                }
+                // case - 04 : 英国商业知识得分 100, 签证知识得分 60-90, 基本信息得分 30-50
+                else if (S3Score == 100 && S2Score >= 60 && S2Score <= 90 && S1Score >= 30 && S1Score <= 50)
+                {
+                    S3ScoreMsg = "您对商业知识非常了解，个人背景有一些基础，对企业家签证也有一定的了解，可阅读《商业创业指南》加深了解英国企业家签证知识后，可尝试申请企业家签证。";
+                }
+                // case - 05 : 英国商业知识得分 60-90, 签证知识得分 100, 基本信息得分 60及以上
+                else if (S3Score >= 60 && S3Score <= 90 && S2Score == 100 && S1Score >= 60)
+                {
+                    S3ScoreMsg = "您的个人背景和签证知识都非常好，大体的商业知识也已了解，可通过阅读《商业创业指南》加深对英国商业的理解，在做好充分的准备工作后可申请英国的企业家签证。";
+                }
+                // case - 06 : 英国商业知识得分 60-90, 签证知识得分 60-90, 基本信息得分 60及以上
+                else if (S3Score >= 60 && S3Score <= 90 && S2Score >= 60 && S2Score <= 90 && S1Score >= 60)
+                {
+                    S3ScoreMsg = "您的个人背景很好，对商业知识和企业家签证也有一定的了解，可通过阅读《商业创业指南》加深对英国商业和企业家签证的理解，在做好充分的准备工作后可尝试申请英国的企业家签证。";
+                }
+                // case - 07 : 英国商业知识得分 60-90, 签证知识得分 100, 基本信息得分 30-50
+                else if (S3Score >= 60 && S3Score <= 90 && S2Score == 100 && S1Score >= 30 && S1Score <= 50)
+                {
+                    S3ScoreMsg = "您对企业家签证非常了解，对英国商业知识也有一定的了解，个人背景也有一定的基础，请阅读《商业创业指南》在深入了解、提高后可尝试申请企业家签证。";
+                }
+                // case - 08 : 英国商业知识得分 60-90, 签证知识得分 60-90, 基本信息得分 30-50
+                else if (S3Score >= 60 && S3Score <= 90 && S2Score >= 60 && S2Score <= 90 && S1Score >= 30 && S1Score <= 50)
+                {
+                    S3ScoreMsg = "您对英国商业知识、企业家签证都有一定的了解，个人背景也有一定的基础，请阅读《商业创业指南》在深入了解、提高后可尝试申请企业家签证。";
+                }
+                // case - 09 : 英国商业知识得分 0-50, 签证知识得分 100, 基本信息得分 60及以上
+                else if (S3Score <= 50 && S2Score == 100 && S1Score >= 60)
+                {
+                    S3ScoreMsg = "您的个人背景和签证知识都非常好，但缺乏英国的商业知识，请阅读《商业创业指南》，深入了解英国商业知识后可尝试申请英国的企业家签证。";
+                }
+                // case - 10 : 英国商业知识得分 0-50, 签证知识得分 60-90, 基本信息得分 60及以上
+                else if (S3Score <= 50 && S2Score >= 60 && S2Score <= 90 && S1Score >= 60)
+                {
+                    S3ScoreMsg = "您的个人背景很好，对企业家签证也有一定的了解，需加强商业知识，可通过阅读《商业创业指南》加深对英国商业和企业家签证的理解，在做好充分的准备工作后可尝试申请英国的企业家签证。";
+                }
+                // case - 11 : 英国商业知识得分 0-50, 签证知识得分 100, 基本信息得分 30-50
+                else if (S3Score <= 50 && S2Score == 100 && S1Score >= 30 && S1Score <= 50)
+                {
+                    S3ScoreMsg = "您对企业家签证非常了解，个人背景也有一定的基础，但缺乏英国商业知识，可在阅读《商业创业指南》加深了解后，可尝试申请企业家签证。";
+                }
+                // case - 12 : 英国商业知识得分 0-50, 签证知识得分 60-90, 基本信息得分 30-50
+                else if (S3Score <= 50 && S2Score >= 60 && S2Score <= 90 && S1Score >= 30 && S1Score <= 50)
+                {
+                    S3ScoreMsg = "您对企业家签证知识有一定的了解，个人背景也有一定的基础，但英国商业知识有待加强，可通过阅读《商业创业指南》加深了解后，再来参加我们的企业家评测。";
                 }
 
-                ViewBag.Section1ScoreTest = S1ScoreTest;
-                ViewBag.Section2Message = S1ScoreTestMsg;
+                ViewBag.Section1Score = S1Score;
+                ViewBag.Section2Score = S2Score;
+                ViewBag.Section3Score = S3Score;
+                ViewBag.Section3Message = S3ScoreMsg;
 
                 return View("Section4b/S4bPageSum");
             }
